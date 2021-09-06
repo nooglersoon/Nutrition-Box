1bc3aa367e376a3cfd90a838e454734af5b2df82
fauziabd <63455298+nooglersoon@users.noreply.github.com>
2021-09-07 06:32:40 +0700

    Initial Commit

---
+++ Nutrition Box/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nutrition Box
+//
+//  Created by Fauzi Achmad B D on 07/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
